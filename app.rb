9ec7255a4d9805b4597e8b90f6088cafe19c6d17
k <k@ksswre.net>
2018-05-07 16:42:32 +0900

    [Add] draw chat statistics graph (since: 2018-01-01)

--- app.rb
+++ app.rb
@@ -30,6 +30,7 @@ class App < Sinatra::Base
         DATE_FORMAT(entry_at, "%Y-%m-%d") as `date`,
         COUNT(DISTINCT user_id) as `count`
       FROM YouTube_Sync_production.user_room_logs
+      WHERE created_at >= '2018-01-01'
       GROUP BY date
       ORDER BY date
     SQL
@@ -60,6 +61,66 @@ class App < Sinatra::Base
         },
         {
           label: 'Users',
+          type: 'number',
+        },
+      ],
+      rows: result.map{|r|
+        {
+          c: [
+            { v: date2js(r[:date]) },
+            { v: r[:count] },
+          ]
+        }
+      }
+    }.to_json
+  end
+  
+  get '/api/v1/status/chat' do
+    content_type :json
+    
+    db = Mysql2::Client.new(
+      database: 'YouTube_Sync_production',
+      host:     ENV['SYNCPOD_DB_HOST'],
+      username: ENV['SYNCPOD_DB_USERNAME'],
+      password: ENV['SYNCPOD_DB_PASSWORD'],
+    )
+    
+    sql =<<~SQL
+      SELECT
+        DATE_FORMAT(created_at, "%Y-%m-%d") as `date`,
+        COUNT(DISTINCT id) as `count`
+      FROM YouTube_Sync_production.chats
+      WHERE created_at >= '2018-01-01'
+      GROUP BY date
+      ORDER BY date
+    SQL
+    
+    data = db.query(sql).map{|r| {date: Date.parse(r['date']), count: r['count']} }.sort_by{|e| e[:date]}
+    start_at = data.first[:date]
+    
+    result = data.inject([]) do |before, v|
+      if before.last && (b = before.last[:date])
+        # if missed point found
+        if v[:date] - b > 1
+          (v[:date] - b - 1).to_i.times do |x|
+            before << {
+              date: (b + x + 1),
+              count: 0,
+            }
+          end
+        end
+      end
+      before << v
+    end
+    
+    {
+      cols: [
+        {
+          label: 'Date',
+          type: 'date',
+        },
+        {
+          label: 'Messages',
           type: 'number',
         },
       ],
